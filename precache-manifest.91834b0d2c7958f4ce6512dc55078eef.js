c67f5328ece436357da0c05ea2fb7117bf111e34
osj <ojsanch@gmail.com>
2020-11-11 17:44:32 -0600

    updated css/media queries

---
+++ precache-manifest.91834b0d2c7958f4ce6512dc55078eef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b206c85828ff9847bdd7f1b0ed0b5460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a171145df9b5962a15e",
+    "url": "/static/css/main.22a38e2c.chunk.css"
+  },
+  {
+    "revision": "2faea4678b7fc01071e3",
+    "url": "/static/js/2.0509dd21.chunk.js"
+  },
+  {
+    "revision": "72c2168a992bae65033dc2d74bcc3eb6",
+    "url": "/static/js/2.0509dd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a171145df9b5962a15e",
+    "url": "/static/js/main.a5d0ba5d.chunk.js"
+  },
+  {
+    "revision": "803bba20a6fa98812a60",
+    "url": "/static/js/runtime-main.8da67116.js"
+  },
+  {
+    "revision": "ba16c1c5df2f1d526708dc5eff5967fa",
+    "url": "/static/media/carbon-2.ba16c1c5.png"
+  }
+]);
